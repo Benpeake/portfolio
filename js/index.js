4173e75e826ae77063b29cb5f646d376ba6f59e8
Ben Peake <dearbenp@gmail.com>
2023-08-08 09:59:52 +0100

    mobile modal menu

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
 const projectButton = document.querySelector('.project-button')
 const projectDrop = document.querySelector('.project-dropdown')
 
+const mobileDisplayButton = document.querySelector('.plus-button')
+const mobileMenu = document.querySelector('.mob-menu')
+const mobilePlus = document.querySelector('.mobile-plus-no-rotate')
+
 projectButton.addEventListener('click', ()=>{
     projectDrop.classList.toggle('project-dropdown-appear')
+})
+
+mobileDisplayButton.addEventListener('click', ()=>{
+    mobileMenu.classList.toggle('mob-menu-apear')
+})
+
+mobilePlus.addEventListener('click', ()=>{
+    mobilePlus.classList.toggle('mobile-plus-rotate')
 })
 
