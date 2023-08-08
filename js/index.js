89c3712782940bc8cbc77272d9d2d5a56ed9e888
Ben Peake <dearbenp@gmail.com>
2023-08-08 10:50:31 +0100

    mobile modal stying

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ const projectDrop = document.querySelector('.project-dropdown')
 const mobileDisplayButton = document.querySelector('.plus-button')
 const mobileMenu = document.querySelector('.mob-menu')
 const mobilePlus = document.querySelector('.mobile-plus-no-rotate')
+const projectSubMenu = document.querySelector('.projects-submenu')
+const mobileProjects = document.querySelector('.project-mobile-menu')
 
 projectButton.addEventListener('click', ()=>{
     projectDrop.classList.toggle('project-dropdown-appear')
@@ -15,5 +17,9 @@ mobileDisplayButton.addEventListener('click', ()=>{
 
 mobilePlus.addEventListener('click', ()=>{
     mobilePlus.classList.toggle('mobile-plus-rotate')
+})
+
+projectSubMenu.addEventListener('click', ()=>{
+    mobileProjects.classList.toggle('project-mobile-menu-appear')
 })
 
