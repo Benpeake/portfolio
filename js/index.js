28121a51e2021535ff10343c899bd662aef735a7
Ben Peake <dearbenp@gmail.com>
2023-09-24 22:17:01 +0100

    tweaks to design and content

--- js/index.js
+++ js/index.js
@@ -7,27 +7,27 @@ const mobilePlus = document.querySelector('.mobile-plus-no-rotate')
 const projectSubMenu = document.querySelector('.projects-submenu')
 const mobileProjects = document.querySelector('.project-mobile-menu')
 
-projectButton.addEventListener('click', ()=>{
+projectButton.addEventListener('click', () =>{
     projectDrop.classList.toggle('project-dropdown-appear')
 })
 
-projectButton.addEventListener('click', ()=>{
+projectButton.addEventListener('click', () =>{
     projectButton.classList.toggle('link-active')
 })
 
-mobileDisplayButton.addEventListener('click', ()=>{
+mobileDisplayButton.addEventListener('click', () =>{
     mobileMenu.classList.toggle('mob-menu-apear')
 })
 
-mobilePlus.addEventListener('click', ()=>{
+mobilePlus.addEventListener('click', () =>{
     mobilePlus.classList.toggle('mobile-plus-rotate')
 })
 
-projectSubMenu.addEventListener('click', ()=>{
+projectSubMenu.addEventListener('click', () =>{
     mobileProjects.classList.toggle('project-mobile-menu-appear')
 })
 
-projectSubMenu.addEventListener('click', ()=>{
+projectSubMenu.addEventListener('click', () =>{
     projectSubMenu.classList.toggle('link-active')
 })
 
@@ -50,8 +50,8 @@ fetch('portfolio.json')
                 <div class="project-image"><img src="` + project.img + `" alt="image placeholder"></div>
                 <div class="project-copy copy">
                     <h2 class="project-title">`+ project.title + `</h2>
-                    <p>`+ project.description +`</p>
-                    <P class="project-links"><a href="#">Read more</a><a href="#">View on Github</a></P>
+                    <p class="project-description">`+ project.description +`</p>
+                    <P class="project-links"><a href="#">Live version</a><a href="#">Github</a></P>
                 </div>
             </div>
         </section>
@@ -62,6 +62,7 @@ fetch('portfolio.json')
         const projectDropMenu = document.querySelector('.project-dropdown')
 
         let menuOutput = "<ul>"
+        
 
         data.projects.forEach(project =>{
 
@@ -71,9 +72,21 @@ fetch('portfolio.json')
 
         })
 
-        const projectDropMenuMob = document.querySelector('.project-mobile-menu')
+        const MobileMenu = document.querySelector('.mobile_menu')
 
-        let mobileProjectMenu = "<ul>"
+        let mobMenuOutput = '<ul>'
+
+        data.projects.forEach(project =>{
+
+            mobMenuOutput += `<li><a class="projects-submenu href="#`+ project.section +`">`+ project.title +`</a></li>`
+
+            MobileMenu.innerHTML = mobMenuOutput + `</ul>`
+
+        })
+
+        const projectDropMenuMob = document.querySelector('.mobile_menu')
+
+        let mobileProjectMenu = `<ul class="mobile-menu-list">`
 
         data.projects.forEach(project =>{
 
@@ -94,77 +107,35 @@ fetch('portfolio.json')
     })
 
 
-// cv pagination indexing
+//cursor switch
+const introPage = document.getElementById('top-of-site');
+const exitPage = document.getElementById('exit-page');
 
-const cv1 = document.querySelector('.cv1')
-const cv2 = document.querySelector('.cv2')
-const cv3 = document.querySelector('.cv3')
-const cv4 = document.querySelector('.cv4')
+const halfway = window.innerHeight / 2;
 
+document.addEventListener('mousemove', function (mouse) {
+    // Get the bounding rectangles for introPage and exitPage
+    const introRect = introPage.getBoundingClientRect();
+    const exitRect = exitPage.getBoundingClientRect();
 
-const pageTarget = document.querySelector('.cvpage')
-
-window.addEventListener("pageshow", ()=>{
-    pageTarget.innerHTML = '<a class="footerlink cvpage green" href="#"> 1/4 </a></div>'
-});
-
-function cvInViewport() {
-    const bounding = cv1.getBoundingClientRect();
-    const bounding2 = cv2.getBoundingClientRect();
-    const bounding3 = cv3.getBoundingClientRect();
-    const bounding4 = cv4.getBoundingClientRect();
-
-    if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) 
-    {
-        pageTarget.innerHTML = '<a class="footerlink cvpage green" href="#"> 1/4 </a></div>'
-        infoTarget.classList.add('green-underlined')
-
-    } else if (bounding2.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-    {
-        pageTarget.innerHTML = '<a class="footerlink cvpage green" href="#"> 2/4 </a></div>'
-        infoTarget.classList.add('green-underlined')
-
-    } else if (bounding3.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-    {
-        pageTarget.innerHTML = '<a class="footerlink cvpage green" href="#"> 3/4 </a></div>'
-        infoTarget.classList.add('green-underlined')
-
-    } else if (bounding4.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-    {
-        pageTarget.innerHTML = '<a class="footerlink cvpage green" href="#"> 4/4 </a></div>'
-        infoTarget.classList.add('green-underlined')
-        
+    // Check if the mouse is within introPage
+    if (
+        mouse.clientY >= introRect.top && mouse.clientY <= introRect.bottom
+    ) {
+        // Mouse is over introPage, use down_cursor.png
+        document.body.style.cursor = `url('images/down_cursor.png'), auto`;
+    } else if (
+        mouse.clientY >= exitRect.top && mouse.clientY <= exitRect.bottom
+    ) {
+        // Mouse is over exitPage, use up_cursor.png
+        document.body.style.cursor = `url('images/up_cursor.png'), auto`;
+    } else if (mouse.clientY < halfway) {
+        // Mouse is above halfway and not over introPage or exitPage
+        document.body.style.cursor = `url('images/up_cursor.png'), auto`;
     } else {
-        pageTarget.innerHTML = "Instagram"
-        infoTarget.classList.remove('green-underlined')
+        // Mouse is below halfway and not over exitPage
+        document.body.style.cursor = `url('images/down_cursor.png'), auto`;
     }
-}
-
- document.querySelector('.snap-scroll').addEventListener('scroll', () => {
-    cvInViewport();
-
-})
-
-
-// cursor switch
-
-
-const halfway = window.innerHeight / 2
-
-document.addEventListener('mousemove', function(mouse) {
-    if (mouse.clientY < halfway) {
-      document.body.style.cursor = `url('/images/up_cursor.png'), auto`
-    } else {
-      document.body.style.cursor = `url('/images/down_cursor.png'), auto`
-    }
-  })
-
-  //info style
-
-  const infoTarget = document.querySelector('.info-target')
-
-  window.addEventListener("pageshow", ()=>{
-    infoTarget.classList.add('green-underlined')
 });
 
  //Email highlight
