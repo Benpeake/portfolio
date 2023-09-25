b06902953acc936333b6b737fe421e006e8204a9
Ben Peake <dearbenp@gmail.com>
2023-09-25 23:15:49 +0100

    updating json content

--- js/index.js
+++ js/index.js
@@ -49,7 +49,7 @@ fetch('portfolio.json')
             <div id = "`+ project.section +`" class="project-overview">
                 <div class="project-image"><img src="` + project.img + `" alt="image placeholder"></div>
                 <div class="project-copy copy">
-                    <h2 class="project-title">`+ project.title + `</h2>
+                    <h2 class="project-title green">`+ project.title + `</h2>
                     <p class="project-description">`+ project.description +`</p>
                     <P class="project-links"><a href="#">Live version</a><a href="#">Github</a></P>
                 </div>
