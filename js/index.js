b187061b29d431a1c5f6af2da1186371ae8698a8
Benpeake <dearbenp@gmail.com>
2023-10-29 22:18:06 +0000

    portfolio 2.0

--- js/index.js
+++ js/index.js
@@ -1,131 +1,61 @@
-const projectButton = document.querySelector('.project-button');
-const projectDrop = document.querySelector('.project-dropdown');
-const mobileDisplayButton = document.querySelector('.plus-button');
-const mobileMenu = document.querySelector('.mob-menu');
-const mobilePlus = document.querySelector('.mobile-plus-no-rotate');
-const projectSubMenu = document.querySelector('.projects-submenu');
-const mobileProjects = document.querySelector('.project-mobile-menu');
+const projectButton = document.querySelector('.project-button')
+const projectDropMenu = document.querySelector('.project-dropdown')
+const mobilePlus = document.querySelector('.mob-top-right')
+const mobileMenu = document.querySelector('.mob-menu-container')
+const mobilePlusIcon = document.querySelector('.mobile-icon')
 
-const dayModeStylesheet = document.createElement('link');
-dayModeStylesheet.rel = 'stylesheet';
-dayModeStylesheet.href = 'css/day-mode.css';
-dayModeStylesheet.id = 'day-mode-stylesheet';
+//day/night mode
+const toggleButton = document.getElementById('checkbox')
+const body = document.body;
+let cursorUp = `url('images/up_cursor.svg'), auto`
+let cursorDown = `url('images/down_cursor.svg'), auto`
 
-const nightModeStylesheet = document.createElement('link');
-nightModeStylesheet.rel = 'stylesheet';
-nightModeStylesheet.href = 'css/night-mode.css';
-nightModeStylesheet.id = 'night-mode-stylesheet';
-
-// pre-load CSS
-document.head.appendChild(dayModeStylesheet);
-document.head.appendChild(nightModeStylesheet);
-
-// Function to set the initial mode based on browser preferences
-function setInitialMode() {
-  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const checkbox = document.getElementById('checkbox');
-  
-  // Check if the browser prefers dark mode
-  if (prefersDarkMode) {
-    // Check if user preference is already saved in localStorage
-    const userPreference = localStorage.getItem('userModePreference');
-    if (userPreference !== 'day') {
-      // Avoid toggling the checkbox if it's already set to night mode
-      checkbox.checked = true; // Night mode by default
-    }
-  }
-}
-
-setInitialMode();
-
-// DAY / NIGHT MODE...
-const checkbox = document.getElementById('checkbox');
-
-checkbox.addEventListener('click', () => {
-  const isDayMode = checkbox.checked;
-
-  // Save user preference in localStorage
-  localStorage.setItem('userModePreference', isDayMode ? 'day' : 'night');
-
-  if (isDayMode) {
-    cursorUp = `url('images/up_cursor_day.png'), auto`;
-    cursorDown = `url('images/down_cursor_day.png'), auto`;
-    // Enable day mode with a delay
-    nightModeStylesheet.classList.add('fade-out');
-    setTimeout(() => {
-      nightModeStylesheet.disabled = true;
-      dayModeStylesheet.disabled = false;
-      dayModeStylesheet.classList.remove('fade-in');
-      setTimeout(() => {
-        dayModeStylesheet.classList.add('fade-in');
-      }, 0);
-    }, 200); 
+toggleButton.addEventListener('click', () => {
+  if (body.classList.contains('light-mode')) {
+    body.classList.remove('light-mode')
+    body.classList.add('dark-mode')
+    cursorUp = `url('images/up_cursor_day.svg'), auto`
+    cursorDown = `url('images/down_cursor_day.svg'), auto`
   } else {
-    cursorUp = `url('images/up_cursor.png'), auto`;
-    cursorDown = `url('images/down_cursor.png'), auto`;
-    // Enable night mode with a delay
-    dayModeStylesheet.classList.add('fade-out');
-    setTimeout(() => {
-      dayModeStylesheet.disabled = true;
-      nightModeStylesheet.disabled = false;
-      nightModeStylesheet.classList.remove('fade-in');
-      setTimeout(() => {
-        nightModeStylesheet.classList.add('fade-in');
-      }, 0);
-    }, 200); 
+    body.classList.remove('dark-mode')
+    body.classList.add('light-mode')
+    cursorUp = `url('images/up_cursor.svg'), auto`
+    cursorDown = `url('images/down_cursor.svg'), auto`
   }
 });
-
-// Add a transition effect to the stylesheets
-dayModeStylesheet.setAttribute('rel', 'stylesheet');
-nightModeStylesheet.setAttribute('rel', 'stylesheet');
-dayModeStylesheet.classList.add('fade');
-nightModeStylesheet.classList.add('fade');
-
-projectButton.addEventListener('click', () =>{
-    projectDrop.classList.toggle('project-dropdown-appear')
-})
-
-projectButton.addEventListener('click', () =>{
-    projectButton.classList.toggle('link-active')
-})
-
-mobileDisplayButton.addEventListener('click', () =>{
-    mobileMenu.classList.toggle('mob-menu-apear')
-})
-
-mobilePlus.addEventListener('click', () =>{
-    mobilePlus.classList.toggle('mobile-plus-rotate')
-})
-
-projectSubMenu.addEventListener('click', () =>{
-    mobileProjects.classList.toggle('project-mobile-menu-appear')
-})
-
-projectSubMenu.addEventListener('click', () =>{
-    projectSubMenu.classList.toggle('link-active')
-})
-
 
 //Fetch JSON
 fetch('portfolio.json')
     .then((response) => response.json())
     .then(data => {
 
+        //project pages
         const projectTarget = document.querySelector('.json-section')
 
         data.projects.forEach(project => {
-
             projectTarget.innerHTML += 
-
         `
         <section class="project">
             <div id = "`+ project.section +`" class="project-overview">
-                <div class="project-image"><img src="` + project.img + `" alt="image placeholder"></div>
+                <div class="project-image">
+                    <img src="` + project.img + `" alt="`+ project.title+ `">
+                </div>
                 <div class="project-copy copy">
-                    <h2 class="project-title">`+ project.title + `</h2>
+                    <div class="title-bar">
+                        <h2 class="project-title">
+                        <a href="`+ project.livelink +`" target="__blank" rel="noopener">
+                        `
+                        + project.title + 
+                        `
+                        </a>
+                        </h2>
+                        <span class="material-symbols-outlined larger-icon">
+                            <a href="`+ project.livelink +`" target="__blank" rel="noopener">
+                                outbound
+                            </a>
+                        </span>
+                    </div>
                     <p class="project-description">`+ project.description +`</p>
-                    <P class="project-links"><a href="`+ project.livelink +`" target="__blank" rel="noopener" >Live version</a><a href="`+ project.github +` "target="__blank" rel="noopener">Github</a></P>
                 </div>
             </div>
         </section>
@@ -133,52 +63,36 @@ fetch('portfolio.json')
 
         })
 
-        const projectDropMenu = document.querySelector('.project-dropdown')
-
-        let menuOutput = "<ul>"
-        
-
+        // project drop-dow   
+        let projectMenuContent = "<ul>"
         data.projects.forEach(project =>{
-
-            menuOutput += `<li><a href="#`+ project.section +`">`+ project.title +`</a></li>`
-
-            projectDropMenu.innerHTML = menuOutput + "</ul>"
-
+            projectMenuContent += `<li><a href="#`+ project.section +`">`+ project.title +`</a></li>`
+            projectDropMenu.innerHTML = projectMenuContent + "</ul>"
         })
 
-        const MobileMenu = document.querySelector('.mobile_menu')
-
-        let mobMenuOutput = '<ul>'
-
-        data.projects.forEach(project =>{
-
-            mobMenuOutput += `<li><a class="projects-submenu href="#`+ project.section +`">`+ project.title +`</a></li>`
-
-            MobileMenu.innerHTML = mobMenuOutput + `</ul>`
-
+        //mob menu pages
+        const mobileMenuTarget = document.querySelector('.mob-menu')
+        data.projects.forEach(project => {
+            mobileMenuTarget.innerHTML+= 
+            ` 
+            <p><a class="mob-project-link" href="#`+ project.section +`">`+ project.title +`</a></p>
+            `
         })
 
-        const projectDropMenuMob = document.querySelector('.mobile_menu')
-
-        let mobileProjectMenu = `<ul class="mobile-menu-list">`
-
-        data.projects.forEach(project =>{
-
-            mobileProjectMenu += `<li><a class = "mobile-project-link" href="#` + project.section + `">` + project.title + `</a></li>`
-
-            projectDropMenuMob.innerHTML = mobileProjectMenu + "</ul>"
-
+        mobileMenuTarget.innerHTML+= 
+        `          
+        <hr class="horizontal-line">
+        <p><a class="mob-nav-link" href="mailto:benpeake.dev@gmail.com" target="_blank">Email</a></p>
+        <p><a class="nav-link" href="https://github.com/Benpeake" target="_blank" rel="noopener">Github</a></p>
+        `
+        document.querySelectorAll('.mob-project-link').forEach(link =>{
+            link.addEventListener('click', () =>{
+                mobileMenu.classList.toggle('mob-menu-apear')
+                mobilePlus.classList.toggle('mobile-icon-rotate')
         })
-
-        const mobileProjectLink = document.querySelectorAll('.mobile-project-link')
-
-        document.querySelectorAll('.mobile-project-link').forEach(link =>{
-            link.addEventListener('click', ()=>{
-                mobileMenu.classList.remove('mob-menu-apear')
-                mobilePlus.classList.toggle('mobile-plus-rotate')
-            })
         })
     })
+
 
 
 //cursor switch
@@ -187,31 +101,32 @@ const exitPage = document.getElementById('exit-page');
 
 const halfway = window.innerHeight / 2;
 
-let cursorUp = `url('images/up_cursor.png'), auto`
-let cursorDown = `url('images/down_cursor.png'), auto`
-
 document.addEventListener('mousemove', function (mouse) {
     // Get the bounding rectangles for introPage and exitPage
     const introRect = introPage.getBoundingClientRect();
     const exitRect = exitPage.getBoundingClientRect();
 
-    // Check if the mouse is within introPage
     if (
         mouse.clientY >= introRect.top && mouse.clientY <= introRect.bottom
     ) {
-        // Mouse is over introPage, use down_cursor.png
         document.body.style.cursor = cursorDown
     } else if (
         mouse.clientY >= exitRect.top && mouse.clientY <= exitRect.bottom
     ) {
-        // Mouse is over exitPage, use up_cursor.png
         document.body.style.cursor = cursorUp 
     } else if (mouse.clientY < halfway) {
-        // Mouse is above halfway and not over introPage or exitPage
         document.body.style.cursor = cursorUp 
     } else {
-        // Mouse is below halfway and not over exitPage
         document.body.style.cursor = cursorDown
     }
-});
+})
+
+projectButton.addEventListener('click', () =>{
+    projectDropMenu.classList.toggle('project-dropdown-appear')
+})
+
+mobilePlus.addEventListener('click', () =>{
+    mobileMenu.classList.toggle('mob-menu-apear')
+    mobilePlus.classList.toggle('mobile-icon-rotate')
+})
 
